a41701abb044ec975140a6b574672913d7a96e35
forkbattery <forkbattery@gmail.com>
2014-12-21 03:42:44 -0500

    first commit

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by Kanishka Goel on 2014-12-21.
+//  Copyright (c) 2014 Kanishka Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BullsEye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BullsEye
+//
+//  Created by Kanishka Goel on 2014-12-21.
+//  Copyright (c) 2014 Kanishka Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
